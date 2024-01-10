824ff4b3bc731f60f05d0b7020f5c8736dab34f2
Jonas Jelonek <jonas.jelonek@proton.me>
2024-01-10 12:22:43 +0100

    add support for SecureOn password
    Signed-off-by: Jonas Jelonek <jonas.jelonek@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
-use std::io::Write;
+use std::str::FromStr;
 use pnet::datalink::{self, Channel::Ethernet};
 use pnet::datalink::NetworkInterface;
-use pnet::packet::{Packet, ethernet::MutableEthernetPacket};
+use pnet::packet::{Packet, ethernet::{MutableEthernetPacket, EtherTypes}};
+use arrayvec::{self, ArrayVec};
 
 use clap::Parser;
 
+#[derive(Debug, Clone)]
+pub enum WolPassword {
+	SixByte(pnet::util::MacAddr),
+	FourByte(std::net::Ipv4Addr),
+	String(String),
+}
+impl From<&str> for WolPassword {
+	fn from(value: &str) -> Self {
+		if let Ok(ip) = std::net::Ipv4Addr::from_str(value) {
+			return Self::FourByte(ip);
+		}
+		if let Ok(mac) = pnet::util::MacAddr::from_str(value) {
+			return Self::SixByte(mac);
+		}
+		
+		Self::String(value.to_string())
+	}
+}
+
 #[derive(Debug, Parser)]
 pub struct CliArgs {
-	#[arg(short, long)]
+	#[arg(short, long, help = "The MAC address of the target host.")]
 	pub target: pnet::util::MacAddr,
 
 	#[arg(short, long)]
 	pub interface: Option<String>,
+
+	#[arg(
+		short, long,
+		help = "Password (SecureOn) to be used. This can be in IP notation (XXX.XXX.XXX.XXX; 4-byte), MAC notation (FF:FF:FF:FF:FF:FF; 6-byte) or an ASCII-string (length 1-6 characters).",
+	)]
+	pub password: Option<WolPassword>,
 }
 
 fn main() -> Result<(), String> {
@@ -39,13 +65,41 @@ fn main() -> Result<(), String> {
 				};
 		}
 	}
+
+	let mut password: [u8; 6] = [0; 6];
+	let mut pw_short: Option<bool> = None;
+	if let Some(pw) = args.password {
+		match pw {
+			WolPassword::FourByte(pw_f) => {
+				pw_short = Some(true);
+				password[0..=3].copy_from_slice(pw_f.octets().as_slice());
+			},
+			WolPassword::SixByte(pw_m) => {
+				pw_short = Some(false);
+				password.copy_from_slice(pw_m.octets().as_slice());
+			},
+			WolPassword::String(pw_s) => {
+				if pw_s.len() > 6 { return Err(format!("Password is limited to 6 characters!")); }
+				if !pw_s.is_ascii() { return Err(format!("Password")) }
+
+				pw_short = Some(false);
+				password.copy_from_slice(&pw_s[0..=5].as_bytes());
+			},
+		}
+	}
+
 	println!("Sending to {} on iface {}", args.target, interface.name);
 
-	let mut pkt: [u8; 120] = [0; 120];
-	let mut pkt_ref = &mut pkt[14..];
-	pkt_ref.write([ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff ].as_slice()).unwrap();
+	let mut pkt = ArrayVec::<u8, 122>::new(); 
+	pkt.extend([0u8; 14]);
+	pkt.extend([ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff ]);
 	for _ in 0..16 {
-		pkt_ref.write(args.target.octets().as_slice()).unwrap();
+		pkt.extend(args.target.octets());
+	}
+	match pw_short {
+		Some(true) => pkt.try_extend_from_slice(&password[..=3]).unwrap(),
+		Some(false) => pkt.extend(password),
+		None => (),
 	}
 
 	let (mut tx, _) = match datalink::channel(&interface, Default::default()) {
@@ -57,7 +111,7 @@ fn main() -> Result<(), String> {
 	let mut eth_pkt = MutableEthernetPacket::new(&mut pkt[..]).unwrap();
 	eth_pkt.set_destination(args.target);
 	eth_pkt.set_source(interface.mac.unwrap());
-	eth_pkt.set_ethertype(pnet::packet::ethernet::EtherTypes::WakeOnLan);
+	eth_pkt.set_ethertype(EtherTypes::WakeOnLan);
 
 	tx.send_to(eth_pkt.packet(), None)
 		.unwrap() /* Currently, send_to always return Some(_) */
