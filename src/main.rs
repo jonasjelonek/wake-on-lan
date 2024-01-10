cd1694ec43b84214376a4ff6e44fef5c610d5fdf
Jonas Jelonek <jonas.jelonek@proton.me>
2024-01-10 10:27:22 +0100

    optimize implementation
    This commit includes some optimizations, e.g. switching to clap argument
    parser, eliminating all allocating (except for String) for the packet,
    using one buffer for packet building and sending, and setting
    release-profile options for cargo to reduce binary size.
    
    Signed-off-by: Jonas Jelonek <jonas.jelonek@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,65 @@
-use std::str::FromStr;
+use std::io::Write;
 use pnet::datalink::{self, Channel::Ethernet};
-use pnet::datalink::{NetworkInterface, EtherType};
+use pnet::datalink::NetworkInterface;
 use pnet::packet::{Packet, ethernet::MutableEthernetPacket};
-use pnet::util::MacAddr;
 
-fn main() {
-	let mut args = std::env::args();
-	let mut mac_addr: MacAddr = MacAddr(0xff, 0xff, 0xff, 0xff, 0xff, 0xff);
-	let mut interface: Option<String> = None;
-	while let Some(arg) = args.next() {
-		if &arg[..] == "-t" || &arg[..] == "--target" {
-			mac_addr = MacAddr::from_str(&(args.next().unwrap())[..]).unwrap();
-		} else if &arg[..] == "-i" || &arg[..] == "--interface" {
-			interface = Some(args.next().unwrap());
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub struct CliArgs {
+	#[arg(short, long)]
+	pub target: pnet::util::MacAddr,
+
+	#[arg(short, long)]
+	pub interface: Option<String>,
+}
+
+fn main() -> Result<(), String> {
+	let args = CliArgs::parse();
+
+	let interface: NetworkInterface;
+	match args.interface {
+		Some(iface) => {
+			let mut matching_ifaces = datalink::interfaces()
+				.into_iter()
+				.filter(|r#if| r#if.name.as_str() == iface.as_str());
+			
+			interface = match matching_ifaces.next() {
+				Some(r#if) => r#if,
+				None => return Err(format!("Interface '{}' does not exist!", iface))
+			};
+		},
+		None => {
+			interface = match datalink::interfaces()
+				.into_iter()
+				.find(|e| e.is_up() && !e.is_loopback() && !e.ips.is_empty()) 
+				{
+					Some(r#if) => r#if,
+					None => return Err(format!("Could not find a default interface that is up, has an IP address and is not a loopback interface!")),
+				};
 		}
 	}
-	if let None = interface {
-		println!("No interface specified!");
-	}
+	println!("Sending to {} on iface {}", args.target, interface.name);
 
-	let mut iface: Option<NetworkInterface> = None;
-	if let Some(name) = interface.as_ref() {
-		let mut matching_ifaces = datalink::interfaces()
-			.into_iter()
-			.filter(|iface| iface.name.as_str() == name.as_str());
-		iface =  matching_ifaces.next();
-	} 
-	if let None = iface.as_ref() {
-		println!("Using default interface!");
-		iface = datalink::interfaces()
-			.into_iter()
-			.find(|e| e.is_up() && !e.is_loopback() && !e.ips.is_empty());
-	}
-	if let None = iface.as_ref() {
-		println!("Found no interface to use for transmission!");
-		return;
-	}
-	println!("Sending to {} on iface {}", mac_addr, iface.as_ref().unwrap().name);
-	/* From here on it's safe to just unwrap 'iface' */
-
-	let mut pkt = vec![ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff ];
+	let mut pkt: [u8; 120] = [0; 120];
+	let mut pkt_ref = &mut pkt[14..];
+	pkt_ref.write([ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff ].as_slice()).unwrap();
 	for _ in 0..16 {
-		pkt.extend(mac_addr.octets());
+		pkt_ref.write(args.target.octets().as_slice()).unwrap();
 	}
 
-	let (mut tx, _) = match datalink::channel(iface.as_ref().unwrap(), Default::default()) {
+	let (mut tx, _) = match datalink::channel(&interface, Default::default()) {
 		Ok(Ethernet(tx, rx)) => (tx, rx),
 		Ok(_) => panic!("Unhandled channel type"),
 		Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
 	};
 
-	let mut pkt_buf: [u8; 128] = [0; 128];
-	let mut eth_pkt = MutableEthernetPacket::new(&mut pkt_buf[..]).unwrap();
-	eth_pkt.set_payload(&pkt[..]);
-	eth_pkt.set_destination(mac_addr);
-	eth_pkt.set_source(iface.unwrap().mac.unwrap());
+	let mut eth_pkt = MutableEthernetPacket::new(&mut pkt[..]).unwrap();
+	eth_pkt.set_destination(args.target);
+	eth_pkt.set_source(interface.mac.unwrap());
 	eth_pkt.set_ethertype(pnet::packet::ethernet::EtherTypes::WakeOnLan);
 
-	tx.send_to(eth_pkt.packet(), None).unwrap();
+	tx.send_to(eth_pkt.packet(), None)
+		.unwrap() /* Currently, send_to always return Some(_) */
+		.map_err(|e| e.to_string())
 }
